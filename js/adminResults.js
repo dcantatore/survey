a0af4802d6362da7313ccaf705cd1059d346befb
Dario Cantatore <dario.cantatore@gmail.com>
2016-12-08 18:36:30 -0500

    manually promisified results request
    that was fun

--- js/adminResults.js
+++ js/adminResults.js
@@ -8,32 +8,55 @@ var url = 'mongodb://localhost/results';
 MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     console.log("Connected");
-   getResults(db).then();
-   console.log("closing");
-//db.close();
+    getResults(db);
 });
 
 
 
 
-function getResults(db){
-    var outputResults = [];
-    //creates array of questions
-    db.collection('results').aggregate([{$unwind: "$surveyResults"},{$group:{_id:"$surveyResults.question"}}], function(err, results) {
-        assert.equal(err, null);
-        results.map(function (obj){
-            // use for in loop to get the quesion text out of each object
-            for (var ids in obj) {
-                //simplify question to variable for multi use
-                var currentQuestion = obj[ids];
-                db.collection('results').aggregate([{$unwind: "$surveyResults"},{$match: { "surveyResults.question": currentQuestion}},{$group:{_id:"$surveyResults.result", count:{$sum:1}}} ],function(err, result) {
-                    assert.equal(err, null);
-                    console.log(currentQuestion);
-                    console.log(result);
-                });
-               // console.log(currentAnswers);
-            }
+function getResults (db){
+    var returnRes = new Promise(function(resolve, reject) {
+        var outputResults = [];
+        var lengthFinal = 0;
+        //creates array of questions
+        //add a $match based on user input here to make sure we are showing the results for the survey they choose
+        db.collection('results').aggregate([{$unwind: "$surveyResults"}, {$group: {_id: "$surveyResults.question"}}], function (err, results) {
+            assert.equal(err, null);
+            var lengthBefore = results.length;
+
+            results.map(function (obj) {
+                // use for in loop to get the quesion text out of each object
+                for (var ids in obj) {
+                    //simplify question to variable for multi use
+                    var currentQuestion = obj[ids];
+                    db.collection('results').aggregate([{$unwind: "$surveyResults"}, {$match: {"surveyResults.question": currentQuestion}}, {
+                        $group: {
+                            _id: "$surveyResults.result",
+                            count: {$sum: 1}
+                        }
+                    }], function (err, result) {
+                        assert.equal(err, null);
+
+                        console.log(currentQuestion);
+                        console.log(result);
+                        lengthFinal++;
+                        console.log(lengthFinal + "   " + lengthBefore);
+                        if (lengthBefore === lengthFinal){
+                            console.log("resolve");
+                            resolve();
+                        }
+                    });
+                    // console.log(currentAnswers);
+                }
+            });
+
+
         });
+
     });
-}
+    returnRes.then(function(){
+        db.close();
+    })
+
+};
 
